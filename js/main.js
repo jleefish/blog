7d8cfe691a8aa0ee199f5879802ae34b91a13051
jleefish <jhlee@jhs-MacBook-Pro.local>
2016-03-14 18:01:10 -0400

    home draft

--- js/main.js
+++ js/main.js
@@ -33,20 +33,20 @@ app.config(['$routeProvider', function ($routeProvider) {
 /**
  * Controls the Blog
  */
-app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
-  console.log("Blog Controller reporting for duty.");
-});
+// app.controller('BlogCtrl', function ( $scope, $location, $http ) {
+//   console.log("Blog Controller reporting for duty.");
+// });
 
 /**
  * Controls all other Pages
  */
-app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+app.controller('PageCtrl', function ( $scope, $location, $http ) {
   console.log("Page Controller reporting for duty.");
 
   // Activates the Carousel
-  $('.carousel').carousel({
-    interval: 5000
-  });
+  // $('.carousel').carousel({
+  //   interval: 5000
+  // });
 
   // Activates Tooltips for Social Links
   $('.tooltip-social').tooltip({
