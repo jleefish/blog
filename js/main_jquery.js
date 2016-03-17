43b28a35d5a4b270fd684478986ac53d3f71186b
jleefish <jhlee@jhs-MacBook-Pro.local>
2016-03-17 19:44:26 -0400

    Typed sentances update

--- js/main_jquery.js
+++ js/main_jquery.js
@@ -8,13 +8,12 @@ $( window ).load(function() {
 
   $(".headline").typed({
     strings: [
-      "I enjoy programming",
       "I'm studying at Sheridan College in Oakville",
-      "I'm focusing on web / mobile application",
-      "I'm learning MEAN stack on my own",
+      "Focusing on web / mobile application",
+      "Learning MEAN stack on my own",
       "Not easy, but interesting !!",            
       "I want to build useful applications",
-      "I'm a programmer"
+      "I'm a programmer."
     ],
     typeSpeed: 60,
     startDelay: 50,
