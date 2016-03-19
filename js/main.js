11309e86257249acb9b5eda12c315b02f41cebd1
jleefish <jhlee@jhs-MacBook-Pro.local>
2016-03-19 16:28:31 -0400

    add bar graph on home.html

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ var app = angular.module('tutorialWebApp', [
 app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
     // Home
-    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    .when("/", {templateUrl: "partials/home.html", controller: "ChartCtrl"})
     // Pages
     .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
     .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
@@ -36,6 +36,99 @@ app.config(['$routeProvider', function ($routeProvider) {
 // app.controller('BlogCtrl', function ( $scope, $location, $http ) {
 //   console.log("Blog Controller reporting for duty.");
 // });
+
+/**
+ * Controls home Page
+ */
+app.controller('ChartCtrl', function ( $scope, $location, $http ) {
+  console.log("Page Controller reporting for duty.");
+
+    $(function () {
+    // Create the chart
+    $('#container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: ''
+        },
+        subtitle: {
+            text: ''
+        },
+        xAxis: {
+            type: 'category',
+            lineWidth: 0,
+            minorGridLineWidth: 0,
+            gridLineColor: 'transparent',
+            labels: {
+                enabled: true
+            },
+            minorTickLength: 0,
+            tickLength: 0
+        },
+        yAxis: {
+            lineWidth: 0,
+            minorGridLineWidth: 0,
+            gridLineColor: 'transparent',
+            title: {
+                text: ''
+            },
+            labels: {
+                enabled: false
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            series: {
+                borderWidth: 0,
+                dataLabels: {
+                    enabled: false
+                }
+            }
+        },
+
+        tooltip: {
+            headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+            pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+        },
+
+        series: [{
+            colorByPoint: true,
+            data: [{
+                name: 'Microsoft Internet Explorer',
+                y: 56.33,
+            }, {
+                name: 'Chrome',
+                y: 24.03,
+            }, {
+                name: 'Firefox',
+                y: 10.38,
+            }, {
+                name: 'Safari',
+                y: 4.77,
+            }, {
+                name: 'Opera',
+                y: 0.91,
+            }]
+        }]
+    });
+});
+    
+    
+    // Activates the Carousel
+  // $('.carousel').carousel({
+  //   interval: 5000
+  // });
+
+  // Activates Tooltips for Social Links
+  $('.tooltip-social').tooltip({
+    selector: "a[data-toggle=tooltip]"
+  })
+});
+
+
 
 /**
  * Controls all other Pages
