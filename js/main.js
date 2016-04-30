e7738cd6cef76cae4232fc23865ca4187ef90cb2
jleefish <jhlee@jhs-MacBook-Pro.local>
2016-04-30 15:00:25 -0400

    link error fix-contact

--- js/main.js
+++ js/main.js
@@ -16,9 +16,9 @@ app.config(['$routeProvider', function ($routeProvider) {
     .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
     .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
     .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
-    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
+    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})*/
     .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
-    .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
+    /*.when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
     .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
     */ 
     // else 404
