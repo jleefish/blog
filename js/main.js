37c94efb3013a61dfaede063beebe6c97e4487ac
lejunghu <lejunghu@gmail.com>
2016-10-23 21:23:34 -0400

    fix graph

--- js/main.js
+++ js/main.js
@@ -119,17 +119,19 @@ app.controller('ChartCtrl', function ( $scope, $location, $http ) {
                 y: 70.05,
                 level:'Jedi',
                 // desc:''
-            }, {
-                name: 'Coffee',
-                y: 80.91,
-                level:'Master',
-                desc:'Nespresso/Hand Drip'
-            }, {
-                name: 'Inline hockey',
-                y: 76.77,
-                level:'Jedi',
-                desc:'Left-handed'
-            },]
+            }
+            // , {
+            //     name: 'Coffee',
+            //     y: 80.91,
+            //     level:'Master',
+            //     desc:'Nespresso/Hand Drip'
+            // }, {
+            //     name: 'Inline hockey',
+            //     y: 76.77,
+            //     level:'Jedi',
+            //     desc:'Left-handed'
+            // }
+            ]
         }]
     });
 });
